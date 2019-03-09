3a8138fd80303b95637365f6655a6996b92f9dae
Kienz <alif.stinky@gmail.com>
2019-03-09 20:43:47 +0700

    explore screen

--- app/src/main/java/com/example/kienz/domaku/MainActivity.java
+++ app/src/main/java/com/example/kienz/domaku/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.kienz.domaku;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // kita set default nya Home Fragment
+        loadFragment(new explore());
+        // inisialisasi BottomNavigaionView
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bn_main);
+        // beri listener pada saat item/menu bottomnavigation terpilih
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    // method untuk load fragment yang sesuai
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fl_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()){
+            case R.id.home_menu:
+                fragment = new explore();
+                break;
+            case R.id.search_menu:
+                fragment = new donasi();
+                break;
+            case R.id.favorite_menu:
+                fragment = new ambil();
+                break;
+        }
+        return loadFragment(fragment);
     }
 }
