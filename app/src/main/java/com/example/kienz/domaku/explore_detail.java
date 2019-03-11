479b8fe44857bc24b34b363a2b8a939df3bc8ea4
Kienz <alif.stinky@gmail.com>
2019-03-11 23:08:09 +0700

    +H-5 V3

--- app/src/main/java/com/example/kienz/domaku/explore_detail.java
+++ app/src/main/java/com/example/kienz/domaku/explore_detail.java
@@ -2,12 +2,27 @@ package com.example.kienz.domaku;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class explore_detail extends AppCompatActivity {
 
+    @BindView(R.id.explore_detail_back)
+    ImageButton backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explore_detail);
+        ButterKnife.bind(this);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }
